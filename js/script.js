1b6c86a4310548a65b84d9dd2206e770cbaea343
Indayani <davanalendra2@gmail.com>
2026-02-07 13:50:17 +0700

    add card p1

--- js/script.js
+++ js/script.js
@@ -109,3 +109,17 @@ document.querySelectorAll(".encrypt").forEach(el => {
         iteration += 0.25;
     }, 30);
 });
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+    card.addEventListener("mousemove", (e) => {
+        const rect = card.getBoundingClientRect();
+
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        card.style.setProperty("--x", `${x}px`);
+        card.style.setProperty("--y", `${y}px`);
+    });
+});
