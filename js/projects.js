f1b4043d918164c201df91e94fa63aff603460aa
Indayani <davanalendra2@gmail.com>
2026-02-12 13:56:56 +0700

    add projects

---
+++ js/projects.js
@@ -0,0 +1,82 @@
+const btn = document.getElementById("langBtn");
+const dropdown = document.getElementById("langDropdown");
+const currentLang = document.getElementById("currentLang");
+const items = document.querySelectorAll(".lang-item");
+
+btn.addEventListener("click", () => {
+    dropdown.classList.toggle("show");
+    btn.classList.toggle("active");
+});
+
+items.forEach(item => {
+    item.addEventListener("click", () => {
+        const lang = item.dataset.lang;
+
+        currentLang.textContent = lang.toUpperCase();
+
+        items.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+
+        dropdown.classList.remove("show");
+        btn.classList.remove("active");
+
+        localStorage.setItem("lang", lang);
+    });
+});
+
+document.addEventListener("click", (e) => {
+    if (!e.target.closest(".lang")) {
+        dropdown.classList.remove("show");
+    }
+});
+
+const saved = localStorage.getItem("lang");
+if(saved){
+    currentLang.textContent = saved.toUpperCase();
+}
+
+document.querySelectorAll(".ripple-btn").forEach(button => {
+
+    const ripple = button.querySelector(".ripple");
+
+    function moveRipple(e) {
+        const rect = button.getBoundingClientRect();
+        const size = Math.max(rect.width, rect.height) * 2;
+
+        ripple.style.width = size + "px";
+        ripple.style.height = size + "px";
+
+        ripple.style.left = e.clientX - rect.left - size/2 + "px";
+        ripple.style.top = e.clientY - rect.top - size/2 + "px";
+    }
+
+    button.addEventListener("mouseenter", (e) => {
+        moveRipple(e);
+        ripple.classList.remove("leave");
+        ripple.classList.add("enter");
+    });
+
+    button.addEventListener("mousemove", moveRipple);
+
+    button.addEventListener("mouseleave", (e) => {
+        moveRipple(e);
+        ripple.classList.remove("enter");
+        ripple.classList.add("leave");
+    });
+});
+
+const hamburger = document.getElementById("hamburgerBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobileMenu.classList.toggle("show");
+    navMenu.classList.toggle("show");
+});
+
+document.querySelectorAll(".mobile-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        mobileMenu.classList.remove("show");
+    });
+});
